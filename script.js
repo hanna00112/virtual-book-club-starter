4a28506d076914fe02eca8dec93f22db4a812241
Hanna Abrahem <habrahem@haverford.edu>
2024-06-11 10:38:39 -0700

    added some javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function loadReviews() {
+    console.log("LoadReviews called!")
+}
+
+function handleReviewSubmit {
+    console.log("handleReviewSubmit")
+}
+
+document.addEventListener("click", () => {
+    // Do something when the user clicks anywhere on the document
+  });
+  
